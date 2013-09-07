51e03d1d69de2f4b6422462885727e0497913df6
Jason Teplitz <jteplitz602@gmail.com>
2013-09-07 01:27:28 -0400

    up to speed for the submodule

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,21 @@
   "use strict";
 
   var Routes = {
-    Root: require("./Root.js")
+    Root: require("./Root.js"),
+    Signup: require("./Signup.js"),
+    Login: require("./Login.js")  ,
+    AdminShow: require("./AdminShow.js")  ,
+    AdminEpisode: require("./AdminEpisode.js")
   };
 
-  // routes, function, mongo, conf, methods
+  // routes, function, mongo, conf, auth, methods
   var routeList = [
-    ["/", Routes.Root, 0, 0, ["get"]]
+    ["/",       Routes.Root,   0, 0, 0,  ["get"         ]],
+    ["/signup", Routes.Signup, 1, 1, -1, ["get", "post" ]],
+    ["/login",  Routes.Login,  1, 1, 0,  ["get", "post" ]],
+
+    ["/a/show",              Routes.AdminShow,    1, 1, 2,  ["get", "post" ]],
+    ["/a/show/:id/episode",  Routes.AdminEpisode, 1, 0, 2,  ["get", "post" ]]
   ];
 
   exports.routes = routeList;
